c80ded542c0263709f775a3faecb89be4bbd3c88
coolwizard11@gmail.com <h-izu@h-izu.local>
2018-08-17 14:47:29 +0900

    gzip compression

--- chapter6/client.go
+++ chapter6/client.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bufio"
+	"compress/gzip"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"net/http/httputil"
+	"os"
 	"strings"
 )
 
@@ -31,16 +34,32 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
+		// gzip圧縮
+		request.Header.Set("Accept-Encoding", "gzip")
+
 		request.Write(conn)
 		response, err := http.ReadResponse(bufio.NewReader(conn), request)
 		if err != nil {
 			panic(err)
 		}
-		dump, err := httputil.DumpResponse(response, true)
+
+		dump, err := httputil.DumpResponse(response, false)
 		if err != nil {
 			panic(err)
 		}
 		fmt.Println(string(dump))
+		defer response.Body.Close()
+
+		if response.Header.Get("Content-Encoding") == "gzip" {
+			reader, err := gzip.NewReader(response.Body)
+			if err != nil {
+				panic(err)
+			}
+			io.Copy(os.Stdout, reader)
+			reader.Close()
+		} else {
+			io.Copy(os.Stdout, response.Body)
+		}
 
 		current++
 		if current == len(sendMessages) {
